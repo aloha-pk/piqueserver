51ccbcf6054a396542894535cd5a333e3268c890
MuffinTastic <muffintastic32@gmail.com>
2022-03-26 14:26:21 -0700

    change default auth backend name for consistency

--- piqueserver/server.py
+++ piqueserver/server.py
@@ -95,7 +95,7 @@ respawn_time_option = config.option(
 respawn_waves = config.option('respawn_waves', default=False)
 game_mode = config.option('game_mode', default='ctf')
 random_rotation = config.option('random_rotation', default=False)
-auth_backend = config.option('auth_backend', default='piqueserver.auth.ConfigAuthBackend')
+auth_backend = config.option('auth_backend', default='piqueserver.auth.DefaultAuthBackend')
 logfile = logging_config.option('logfile', default='./logs/log.txt')
 loglevel = logging_config.option('loglevel', default='info')
 map_rotation = config.option('rotation', default=['classicgen', 'random'],
