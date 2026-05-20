2ed70eb2c8a19021851385bdde2ca1baf984afea
totallynotaburner <davidco7777@protonmail.com>
2026-05-19 21:23:16 -0600

    Kill arbitrary chat length limit

--- pyspades/player.py
+++ pyspades/player.py
@@ -667,13 +667,13 @@ class ServerConnection(BaseConnection):
         value: str = contained.value
         value = value.translate(MSG_SPECIAL_CHARACTER_MAP)
 
-        if len(value) > 108:
+        if len(value) > 255:
             log.info("TOO LONG MESSAGE ({chars} chars) FROM {name} (#{id})",
                      chars=len(value),
                      name=self.name,
                      id=self.player_id)
 
-        value = value[:108]
+        value = value[:255]
         if value.startswith('/'):
             self.on_command(*parse_command(value[1:]))
         else:
@@ -734,7 +734,7 @@ class ServerConnection(BaseConnection):
     @register_packet_handler(loaders.VersionResponse)
     def on_version_info_recieved(self, contained: loaders.VersionResponse) -> None:
         self.client_info["version"] = contained.version
-        self.client_info["os_info"] = contained.os_info[:108]
+        self.client_info["os_info"] = contained.os_info[:255]
         client_names = {
             ord('a') : "ACE",
             ord('o') : "OpenSpades",
@@ -752,7 +752,7 @@ class ServerConnection(BaseConnection):
             # BetterSpades currently sends the client name in the OS info to
             # deal with old scripts that don't recognize the 'B' indentifier
             match = re.match(r"\ABetterSpades \((.*)\)\Z",
-                             contained.os_info[:108])
+                             contained.os_info[:255])
             if match:
                 self.client_info["os_info"] = match.groups()[0]
         # send extension info to clients that support this packet.
